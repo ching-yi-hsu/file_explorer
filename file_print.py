3897f404684844eeeede6096ec5cf7358424e285
ching-yi-hsu <kup1125@outlook.com>
2020-11-15 00:08:46 +0800

    different label and clicks now for dir and files

---
+++ file_print.py
@@ -0,0 +1,7 @@
+def file_print(f,f_sp):
+    if len(f_sp) == 2 :
+        print(f"this is file, file name is {f_sp[0]}")
+    else:
+        print(f"this is folder, folder name is {f_sp[0]}")
+if __name__ == "__main__":
+    pass
--- main.py
+++ main.py
@@ -1,32 +1,33 @@
 from os import listdir
 import os
 import tkinter as tk
+import file_print
 
+def tk_window():
+    window = tk.Tk()
+    window.title('File Explore')
+    window.geometry('600x500')
+    mypath = os.getcwd()
+    files = listdir(mypath)
+    file_fram = []
+    file_label = []
+    file_boutton = []
 
-
-window = tk.Tk()
-window.title('File Explore')
-window.geometry('500x300')
-mypath = os.getcwd()
-files = listdir(mypath)
-file_fram = []
-file_label = []
-file_boutton = []
-n  = 0
-
-
-for f in files:
+    for f in files:
+        file_fram= tk.Frame(window,height=5,bd = 5 ,background = "pink")
+        f_sp = f.split(".")
+        if len(f_sp) == 2 :
+            file_boutton = tk.Button(file_fram, text = "entery", command = lambda f = f, f_sp = f_sp:file_print.file_print(f,f_sp) )
+            file_boutton.pack(side = "right")
+            file_label = tk.Label(file_fram, text = f )
+            file_label.pack(side = "left")
+        else:
+            file_boutton = tk.Button(file_fram, text = "entery", command = lambda f = f, f_sp = f_sp:file_print.file_print(f,f_sp) )
+            file_boutton.pack(side = "right")
+            file_label = tk.Label(file_fram, text = f, background = "green")
+            file_label.pack(side = "left")
+        file_fram.pack(fill="x", padx=5, pady=5)
+    window.mainloop()
     
-    file_fram= tk.Frame(window,height=5,bd = 5 ,background = "pink")
-    file_label = tk.Label(file_fram, text = f)
-    
-    file_boutton = tk.Button(file_fram, text = "entery", command = lambda name = f:print(name) )
-    file_boutton.pack(side = "right")
-    file_label.pack(side = "left")
-    file_fram.pack(fill="x", padx=5, pady=5)
-  
-    
-    #file_labels.appen(file_label)
-    
-window.mainloop()
-
+if __name__ == "__main__":
+    tk_window()
